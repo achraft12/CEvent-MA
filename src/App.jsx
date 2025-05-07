c7e1417b555a6e8e03ea8c2189d8e0272ad73901
achraft12 <achrafachraftaki@gmail.com>
2025-05-07 16:34:06 +0100

    add event page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import EventList from './pages/Event_List.jsx';
 import EventDetails from './pages/EventDetails.jsx';
 import AboutUs from './pages/AboutUs.jsx';
 import Profile from './pages/Profile.jsx';
+import AddEvent from './pages/AddEvent.jsx';
 
 import './App.css';
 
@@ -29,6 +30,7 @@ function App() {
           <Route path="/event_list" element={<EventList />} />
           <Route path="/events/:id" element={<EventDetails />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/addevent" element={<AddEvent />} />
           <Route path="*" element={<h1>404 Not Found</h1>} />
 
         </Routes>
